a60ddad9808d4c6987343df5d4846001ed4eef7a
Carmelo Santana <me@carmelosantana.com>
2020-12-03 22:36:11 -0500

    Moved default values
    Added better support for select boxes

--- inc/admin.php
+++ inc/admin.php
@@ -4,165 +4,31 @@ declare(strict_types=1);
 use Carbon_Fields\Container;
 use Carbon_Fields\Field;
 
-function spacepress_default_options( string $option ): array {
-    switch ( $option ){
-        case 'dashboard':
-            return [
-                [
-                    'name' => 'mood',                        
-                    'label' => 'Mood',
-                    'type' => 'text',
-                    'set_width' => 50,
-                ],
-                [
-                    'name' => 'emoji',                        
-                    'label' => '',
-                    'type' => 'text',
-                    'set_width' => 50,
-                ],                
-                [
-                    'name' => 'description',                        
-                    'label' => 'Description',
-                    'type' => 'text',
-                ],    
-                [
-                    'name' => 'motd',                        
-                    'label' => 'Message of the day',
-                    'type' => 'textarea',
-                ],                
-            ];
-
-        case 'details':
-            return [
-                [
-                    'name' => 'status',                        
-                    'label' => 'Status',
-                    'type' => 'text',
-                ],
-                [
-                    'name' => 'here_for',                        
-                    'label' => 'Here for',
-                    'type' => 'text',
-                ],
-                [
-                    'name' => 'hometown',                        
-                    'label' => 'Hometown',
-                    'type' => 'text',
-                ],
-                [
-                    'name' => 'ethnicity',                        
-                    'label' => 'Ethnicity',
-                    'type' => 'text',
-                ],
-                [
-                    'name' => 'zodiac_sign',                        
-                    'label' => 'Zodiac Sign',
-                    'type' => 'text',
-                ],
-                [
-                    'name' => 'smoke_drink',                        
-                    'label' => 'Smoke / Drink',
-                    'type' => 'text',
-                ],
-                [
-                    'name' => 'education',                        
-                    'label' => 'Education',
-                    'type' => 'text',
-                ],
-                [
-                    'name' => 'occupation',                        
-                    'label' => 'Occupation',
-                    'type' => 'text',
-                ],
-            ];
-
-        case 'interests':
-            return [
-                [
-                    'name' => 'general',
-                    'label' => 'General',
-                    'type' => 'rich_text',
-                ],
-                [
-                    'name' => 'music',
-                    'label' => 'Music',
-                    'type' => 'rich_text',
-                ],
-                [
-                    'name' => 'movies',
-                    'label' => 'Movies',
-                    'type' => 'rich_text',
-                ],
-                [
-                    'name' => 'television',
-                    'label' => 'Television',
-                    'type' => 'rich_text',
-                ],
-                [
-                    'name' => 'books',
-                    'label' => 'Books',
-                    'type' => 'rich_text',
-                ],
-                [
-                    'name' => 'heroes',
-                    'label' => 'Heroes',
-                    'type' => 'rich_text',
-                ],        
-            ];
-
-            case 'layout':
-                return [
-                    [
-                        'name' => 'top_8_limit',                        
-                        'label' => 'How many in your top "8"?',
-                        'type' => 'text',
-                    ],
-                    [
-                        'name' => 'blog_limit',                        
-                        'label' => 'How many blog posts to list on profile page?',
-                        'type' => 'text',
-                    ],
-                ];    
-    }
-
-    return [];
-}
-
 function spacepress_carbon_attach_theme_options(){
-    $text_inputs = [
-        'text' => 'text',
-        'textarea' => 'textarea',
-        'rich_text' => 'rich_text',
-    ];
+    // start main options
+    $primary_container = Container::make( 'theme_options', __( 'SpacePress' ) )
+        ->set_icon( 'dashicons-groups' )
+        ->add_tab( __( 'Setup'), [
+            Field::make( 'radio', 'instance', __( 'User Environment' ) )
+            ->set_options( [
+                0 => 'Single site, single user',
+                1 => 'Single site, multiple users',
+                2 => 'BuddyPress',
+            ] ),
+        ])
+        ->add_tab( __( 'Theme'), [
+            Field::make( 'textarea', 'tracking_scripts', __( 'Tracking Script' ) ),
+            Field::make( 'header_scripts', 'header_scripts', __( 'Header Script' ) ),
+            Field::make( 'footer_scripts', 'footer_scripts', __( 'Footer Script' ) ),
+        ]);
 
-    $define_user_meta = [
-        [
-            'name' => 'dashboard',
-            'label' => 'Dashboard',
-            'set_options' => $text_inputs,
-            'set_default_value' => spacepress_default_options( 'dashboard' ),
-        ],
-        [
-            'name' => 'interests',
-            'label' => 'Interests',
-            'set_options' => $text_inputs,
-            'set_default_value' => spacepress_default_options( 'interests' ),
-        ],
-        [
-            'name' => 'details',
-            'label' => 'Details',
-            'set_options' => $text_inputs,
-            'set_default_value' => spacepress_default_options( 'details' ),
-        ]        
-    ];
+    // add new container for user options
+    $user_options_container = Container::make( 'theme_options', __( 'User Options' ) )
+        ->set_page_parent( $primary_container );
 
-    $container = Container::make( 'theme_options', __( 'SpacePress' ) )
-        // ->set_page_menu_position( 60 )
-        // ->set_page_menu_title( 'Options' )
-        ->set_icon( 'dashicons-groups' );
-
-    foreach ( $define_user_meta as $option ){
-        $container->add_tab( __( $option['label'] ), [
+    // builds user options
+    foreach ( spacepress_get_user_options() as $option ){
+        $user_options_container->add_tab( __( $option['label'] ), [
             Field::make( 'complex', $option['name'], $option['label'] )
             ->add_fields( [                
                 Field::make( 'text', 'name', __( 'Name' ) )
@@ -172,23 +38,49 @@ function spacepress_carbon_attach_theme_options(){
                 Field::make( 'text', 'set_width', __( 'Width' ) )
                 ->set_width( 50 ),
                 Field::make( 'select', 'type', __( 'Type' ) )
-                ->set_width( 50 )                
-                ->set_options( $option['set_options'] ),
-                ] )
-                ->set_default_value( $option['set_default_value'] )              
+                ->set_width( 50 )
+                ->set_options( spacepress_get_input_options() ),
+                Field::make( 'select', 'select_values', __( 'Select Values' ) )
+                ->set_width( 50 )
+                ->set_options( spacepress_get_select_groups() ),
+            ] )
+            ->set_default_value( $option['set_default_value'] )              
         ]);   
-    }  
+    }
+}
 
-    $user = Container::make( 'user_meta', __( 'About Me' ) );
-    foreach ( $define_user_meta as $id ){
-        $user->add_fields( [
+function spacepress_carbon_attach_user_meta(){  
+    // return;  
+    // start user meta container
+    $user_meta_container = Container::make( 'user_meta', __( 'About Me' ) );
+
+    foreach ( spacepress_get_user_options() as $id ){
+        // separate sections
+        $user_meta_container->add_fields( [
             Field::make( 'separator', $id['name'], $id['label'] )
-        ]); 
+        ]);
+        
+        // build user meta options
         foreach ( carbon_get_theme_option( $id['name'] ) as $option ){
-            $user->add_fields( [
-                Field::make( $option['type'], $option['name'], __( $option['label'] ) )
-                ->set_width( $option['set_width'] ?? 100 ),
-            ] );
+            if ( empty($option['name']) or empty($option['type']) )
+                continue;
+
+            switch ( $option['type'] ){
+                case 'select':
+                    $user_meta_container->add_fields( [
+                        Field::make( $option['type'], $option['name'], __( $option['label'] ) )
+                        ->set_width( $option['set_width'] ?? 100 )
+                        ->set_options( spacepress_get_select_values( $option['select_values'] ) )
+                    ] );                    
+                break;
+
+                default:
+                    $user_meta_container->add_fields( [
+                        Field::make( $option['type'], $option['name'], __( $option['label'] ) )
+                        ->set_width( $option['set_width'] ?? 100 )
+                    ] );
+                break;
+            }
         }
     }
 
@@ -209,9 +101,8 @@ function spacepress_carbon_attach_theme_options(){
     //         ->set_storage_format( 'Y' )
     //         ->set_width( 40 )
     //         ] )     
-    // ]);
+    // ]);    
 }
-
 
 function spacepress_carbon_load() {
     \Carbon_Fields\Carbon_Fields::boot();
