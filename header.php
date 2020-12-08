f398fcb30518b717f4fb4fa4e59d8df901348b8f
Carmelo Santana <me@carmelosantana.com>
2020-12-08 16:16:59 -0500

    Added WP_Bootstrap_Navwalker menu
    Added top ads space

--- header.php
+++ header.php
@@ -12,48 +12,44 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<?php wp_head(); ?>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'spacepress' ); ?></a>
-
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$spacepress_description = get_bloginfo( 'description', 'display' );
-			if ( $spacepress_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $spacepress_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'spacepress' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+    <?php wp_body_open(); ?>
+    <div class="container">
+        <header class="sp-header">
+            <?php get_template_part( 'template-parts/widget-ads' ); ?>
+        </header>
+        <nav class="navbar navbar-expand-lg sp-primary-menu" role="navigation">
+            <div class="container-fluid">
+                <button class="navbar-toggler bg-light" type="button" data-toggle="collapse"
+                    data-target="#sp-primary-menu" aria-controls="sp-primary-menu"
+                    aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'spacepress' ); ?>">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <?php
+                    wp_nav_menu( [ 
+                        'theme_location' => 'menu-1',
+                        'menu_id' => 'primary-menu',
+                        'depth' => 2,
+                        'container' => 'div',
+                        'container_class' => 'collapse navbar-collapse',
+                        'container_id' => 'sp-primary-menu',
+                        'menu_class' => 'navbar-nav mr-auto mb-2 mb-lg-0',
+                        'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
+                        'walker' => new WP_Bootstrap_Navwalker(),
+                    ] );
+                ?>
+                <form class="d-flex">
+                    <input class="form-control mr-2 p-1 rounded-0 border-0 lh-1" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn rounded-0 border-0 btn-light" type="submit">Search</button>
+                </form>
+            </div>
+        </nav>      
+        <div class="row sp-body-wrap">
