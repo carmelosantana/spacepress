4ad90dffb7bf057eb1c811a666a5738b60e280a8
Carmelo Santana <me@carmelosantana.com>
2020-12-08 16:24:16 -0500

    Moved sidebar to left
    Added template parts to build profile page

--- index.php
+++ index.php
@@ -13,45 +13,51 @@
  */
 
 get_header();
-?>
-
-	<main id="primary" class="site-main">
-
-		<?php
-		if ( have_posts() ) :
-
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
-			endif;
-
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-	</main><!-- #main -->
-
-<?php
 get_sidebar();
+?>
+<div class="col-md-8 float-right">
+    <?php
+    get_template_part( 'template-parts/widget-user-blockquote' );
+    get_template_part( 'template-parts/widget-user-latest' );     
+    get_template_part( 'template-parts/widget-user-blurbs' );
+    get_template_part( 'template-parts/widget-user-top' );
+    ?>
+    <main id="primary" class="site-main">
+
+    <?php
+    if ( have_posts() ) :
+
+        if ( is_home() && ! is_front_page() ) :
+            ?>
+            <header>
+                <h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+            </header>
+            <?php
+        endif;
+
+        /* Start the Loop */
+        while ( have_posts() ) :
+            the_post();
+
+            /*
+            * Include the Post-Type-specific template for the content.
+            * If you want to override this in a child theme, then include a file
+            * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+            */
+            get_template_part( 'template-parts/content', get_post_type() );
+
+        endwhile;
+
+        the_posts_navigation();
+
+    else :
+
+        get_template_part( 'template-parts/content', 'none' );
+
+    endif;
+    ?>
+
+    </main><!-- #main -->
+</div>
+<?php
 get_footer();
