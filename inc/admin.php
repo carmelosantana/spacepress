bad0deedeacc77f6b3892a2ad10da93b74e4a018
Carmelo Santana <me@carmelosantana.com>
2020-12-08 16:28:08 -0500

    Added theme options
    Added CDN theme options
    Added Ads to theme options
    Added Top support to theme + user metas
    Added Schools to user metas
    Added spacepress_get_stylesheets()

--- inc/admin.php
+++ inc/admin.php
@@ -5,22 +5,57 @@ use Carbon_Fields\Container;
 use Carbon_Fields\Field;
 
 function spacepress_carbon_attach_theme_options(){
+	
+    
     // start main options
     $primary_container = Container::make( 'theme_options', __( 'SpacePress' ) )
         ->set_icon( 'dashicons-groups' )
-        ->add_tab( __( 'Setup'), [
-            Field::make( 'radio', 'instance', __( 'User Environment' ) )
-            ->set_options( [
-                0 => 'Single site, single user',
-                1 => 'Single site, multiple users',
-                2 => 'BuddyPress',
-            ] ),
-        ])
         ->add_tab( __( 'Theme'), [
+            Field::make( 'select', 'theme', __( 'CSS Theme' ) )
+            ->set_options( spacepress_get_stylesheets() )
+            ->set_default_value( 'classic-responsive' ),
+            Field::make( 'checkbox', 'cdn_bootstrap', __( 'Bootstrap CDN' ) )
+            ->set_option_value( 'yes' )
+            ->set_default_value( 'yes' ),
             Field::make( 'textarea', 'tracking_scripts', __( 'Tracking Script' ) ),
             Field::make( 'header_scripts', 'header_scripts', __( 'Header Script' ) ),
             Field::make( 'footer_scripts', 'footer_scripts', __( 'Footer Script' ) ),
-        ]);
+        ] );
+    
+    // Ads
+    $primary_container->add_tab( __( 'Ads' ), [
+        Field::make( 'complex', 'ads', __( 'Ads' ) )
+        ->add_fields( 'image', [
+            Field::make( 'text', 'name', __( 'Name' ) ),
+            Field::make( 'text', 'url', __( 'URL' ) ),
+            Field::make( 'image', 'image', __( 'Image' ) )
+        ] )
+        ->add_fields( 'script', [
+                Field::make( 'text', 'name', __( 'Name' ) ),
+                Field::make( 'textarea', 'script', __( 'Script' ) ),
+        ] )
+    ] );    
+
+    // Top
+    $primary_container->add_tab( __( 'Top' ), [
+        Field::make( 'complex', 'top', __( 'Top' ) )
+        ->add_fields( 'url', [
+            Field::make( 'text', 'name', __( 'Name' ) )
+            ->set_width( 50 ),
+            Field::make( 'text', 'label', __( 'Label' ) )
+            ->set_width( 50 )
+        ] )
+        ->set_default_value(
+            [
+                0 => [
+                    '_type' => 'url',
+                    'name' => 'friends',
+                    'label' => 'Friends'
+    
+                ]
+            ]
+        )
+    ] );
 
     // add new container for user options
     $user_options_container = Container::make( 'theme_options', __( 'User Options' ) )
@@ -84,24 +119,56 @@ function spacepress_carbon_attach_user_meta(){
         }
     }
 
-    // // enable multiple zones like this, work, study etc 
-    // $user->add_tab( __( 'School' ), [
-    //     Field::make( 'complex', 'school', __( 'School' ) )
-    //     ->add_fields( [
-    //         Field::make( 'text', 'name', __( 'Name' ) )
-    //         ->set_width( 40 ),
-    //         Field::make( 'text', 'location', __( 'Location' ) )
-    //         ->set_width( 40 ),
-    //         Field::make( 'text', 'study', __( 'Study' ) )
-    //         ->set_width( 40 ),
-    //         Field::make( 'date', 'attend_start', __( 'From' ) )
-    //         ->set_storage_format( 'Y' )
-    //         ->set_width( 40 ),
-    //         Field::make( 'date', 'attend_end', __( 'To' ) )
-    //         ->set_storage_format( 'Y' )
-    //         ->set_width( 40 )
-    //         ] )     
-    // ]);    
+    // enable multiple zones like this, work, study etc 
+    foreach ( carbon_get_theme_option( 'top' ) as $option ){
+        switch ( $option['_type'] ){
+            case 'url':
+                $user_meta_container->add_tab( __( $option['label'] ), [
+                    Field::make( 'complex', $option['name'], __( $option['label'] ) )
+                    ->add_fields( [
+                        Field::make( 'text', 'name', __( 'Name' ) )
+                        ->set_width( 50 ),
+                        Field::make( 'text', 'url', __( 'URL' ) ),
+                        Field::make( 'image', 'image', __( 'Image' ) )
+                        ] )     
+                ]);        
+            break;
+        }
+    }
+    
+    // enable multiple zones like this, work, study etc 
+    $user_meta_container->add_tab( __( 'School' ), [
+        Field::make( 'complex', 'school', __( 'School' ) )
+        ->add_fields( [
+            Field::make( 'text', 'name', __( 'Name' ) )
+            ->set_width( 50 ),
+            Field::make( 'text', 'study', __( 'Study' ) )
+            ->set_width( 50 ),
+            Field::make( 'map', 'location', __( 'Location' ) )
+            ->set_width( 100 ),
+            Field::make( 'date', 'attend_start', __( 'From' ) )
+            ->set_storage_format( 'Y' )
+            ->set_width( 50 ),
+            Field::make( 'date', 'attend_end', __( 'To' ) )
+            ->set_storage_format( 'Y' )
+            ->set_width( 50 )
+            ] )     
+    ]);     
+}
+
+function spacepress_get_stylesheets(){
+	$files = new FilesystemIterator(get_stylesheet_directory() . '/styles', FilesystemIterator::SKIP_DOTS);
+    
+    $styles = [
+        0 => 'None',        
+    ];
+
+	while ( $files->valid() ) {
+        $styles[pathinfo( $files->getPathname() )['filename']] = pathinfo( $files->getPathname() )['filename'];
+		$files->next();
+    }
+    
+    return $styles;
 }
 
 function spacepress_carbon_load() {
