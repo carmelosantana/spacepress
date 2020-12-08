f910f8a2c3707570bb76af68b0fc3483800f1ded
Carmelo Santana <me@carmelosantana.com>
2020-12-08 16:34:29 -0500

    Added support for multiple Top sections
    Added declare(strict_types=1)

--- template-parts/widget-user-top.php
+++ template-parts/widget-user-top.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types=1);
+
 $args = extract(wp_parse_args($args, [
     'user_id' => 0,
 ]));
@@ -8,15 +10,25 @@ if ( !$user_id and !$user_id = spacepress_get_user_id() )
     return false;
 ?>
 <section class="sp-top">
-    <p class="sp-heading sp-header-top h6">Top</p>
-    <div class="row">
-        <?php
-            foreach ( carbon_get_user_meta( $user_id, 'top' ) as $option ){
-                echo '<div class="col-3 my-3 mx-auto p-0">';
-                echo '<a href="' . $option['url'] . '" class="">' . $option['name'] . '</a>';
-                echo wp_get_attachment_image( $option['image'] );
-                echo '</div>';
+    <?php
+        foreach ( carbon_get_theme_option( 'top' ) as $top ){
+            echo '<p class="sp-heading h6">' . spacepress_get_display_name( $user_id ) . __( '\'s ', 'spacepress' ) . strtolower( $top['label'] ) . '</p>';
+            echo '<div class="d-flex flex-row">';
+            echo '<div class="col-12">';
+            
+            switch ( $top['_type'] ){
+                case 'url':
+                    foreach ( carbon_get_user_meta( $user_id, $top['name'] ) as $option ){
+                        echo '<div class="float-left sp-image">';
+                        echo '<a href="' . $option['url'] . '" class="d-inline-block">' . $option['name'] . '</a><br>';
+                        echo wp_get_attachment_image( $option['image'] );
+                        echo '</div>';
+                    }        
+                break;
             }
-        ?>
-    </div>
-  </section>
+
+            echo '</div>';
+            echo '</div>';
+        }
+    ?>
+</section>
