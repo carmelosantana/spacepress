d35016a84bd5322a9cbddf00d147d6cd7ad4e88b
Carmelo Santana <me@carmelosantana.com>
2020-12-08 16:32:47 -0500

    Cleaned up blurb titles
    Removed unused code
    Added declare(strict_types=1)

--- template-parts/widget-user-blurbs.php
+++ template-parts/widget-user-blurbs.php
@@ -1,28 +1,24 @@
 <?php
+declare(strict_types=1);
+
 $args = extract(wp_parse_args($args, [
     'user_id' => 0,
-    'name' => null,
-    'label' => null,
 ]));
 
 // if no user id provided and we can't guess, we don't output anything
 if ( !$user_id and !$user_id = spacepress_get_user_id() )
     return false;
-
-if ( !$motd = carbon_get_user_meta( $user_id, 'motd' ) )
-    return false;
-
-$cite = carbon_get_user_meta( $user_id, 'motd_cite' );
 ?>
-<section class="container sp-blurbs">
+<section class="sp-blurbs mt-4">
     <div class="row">
-        <?php
-            foreach ( carbon_get_theme_option( 'blurbs' ) as $option ){
-                echo '<div class="col-12 m-0 p-0">';
-                echo '<h5 class="">' . $option['label'] . '</h5>';
-                echo '<p class="">' . carbon_get_user_meta( $user_id, $option['name'] ) . '</p>';
-                echo '</div>';
-            }
-        ?>
+        <div class="col">
+            <p class="h6 sp-heading "><?php echo spacepress_get_display_name( $user_id ) . __( '\'s latest blurbs', 'spacepress' ); ?>
+            <?php
+                foreach ( carbon_get_theme_option( 'blurbs' ) as $option ){
+                    echo '<dt class="sp-header-' . $option['name'] . ' h6">' . $option['label'] . ':</dt>';
+                    echo '<dd class="sp-details">' . carbon_get_user_meta( $user_id, $option['name'] ) . '</dd>';
+                }
+            ?>
+        </div>
     </div>
-  </section>
+</section>
